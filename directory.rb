02a98e0938a58242f658dac76136d26188abd94c
Sapphire <saphmb@gmail.com>
2017-06-24 17:31:51 +0100

    Create interactive menu

--- directory.rb
+++ directory.rb
@@ -1,6 +1,30 @@
+def interactive_menu
+    students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+
+    selection = gets.chomp
+
+    case selection
+      when "1"
+        students = input_students
+      when "2"
+        print_header
+        print(students)
+        print_footer(students)
+      when "9"
+        exit
+      else
+        puts "I don't know what you meant, try again"
+    end
+  end
+end
+
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  #puts "Please enter the names of the students"
+  #puts "To finish, just hit return twice"
 
   students = []
   accepted_cohort_values = [ :january, :february, :march, :april, :may, :june, :july, :august, :september, :october, :november, :december, :"" ]
@@ -58,8 +82,12 @@ def filter(students)
 end
 
 def print(students)
-  students.each_with_index do |student, index|
-    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)".center(100)
+  if students.empty?
+    puts "Please type a student's name".center(100)
+  else
+    students.each_with_index do |student, index|
+      puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)".center(100)
+    end
   end
 end
 
@@ -71,8 +99,12 @@ def print_footer(names)
   end
 end
 
+interactive_menu
+
 students = input_students
 students = filter(students)
+
+
 
 print_header
 print(students)
