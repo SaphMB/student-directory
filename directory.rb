5cb86316ed01ae70cf3ed8901365269a4fef868c
Sapphire <saphmb@gmail.com>
2017-06-25 02:41:26 +0100

    Add method to load students from students.csv

--- directory.rb
+++ directory.rb
@@ -3,7 +3,8 @@
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "3. Save the list to student.csv"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -21,6 +22,8 @@ def process(selection)
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       exit
     else
@@ -67,7 +70,7 @@ def input_students
     name = gets[0..-2]
   end
   @students
-  end
+end
 
 def print_header
   puts "The students of Villains Academy".center(100)
@@ -99,6 +102,15 @@ def save_students
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
     file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open('students.csv', "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+      @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
 end
