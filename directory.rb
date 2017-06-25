0f41d66f15fbf5cce6d5805779a5621d0f1a2055
Sapphire <saphmb@gmail.com>
2017-06-25 19:53:57 +0100

    Use CSV library

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+require 'csv'
 @students = []
 
 def print_menu
@@ -29,9 +30,10 @@ def process(selection)
       filename = gets.chomp
       save_students(filename)
     when "4"
-      puts "4. Load the list from students.csv"
+      puts "4. Load the list from a file"
       puts "Type the file name"
       filename = gets.chomp
+      puts "Students loaded from #{filename}"
       load_students(filename)
     when "9"
       puts "Exiting program"
@@ -107,23 +109,19 @@ def print_footer
 end
 
 def save_students(filename)
-  File.open(filename, "w") do |file|
+  CSV.open(filename, "w") do |csv|
   @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
+    csv << [student[:name], student[:cohort]]
     end
   end
   puts "Your changes have been saved to #{filename}"
 end
 
 def load_students(filename = "students.csv")
-  File.open(filename, "r") do |file|
-    file.readlines.each do |line|
-      name, cohort = line.chomp.split(",")
+  CSV.foreach(filename) do |row|
+    name, cohort = row
         add_to_student_list(name,cohort)
     end
-  end
 end
 
 def try_load_students
